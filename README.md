1d313d5961421ae947ce161c28df612a55f0ab5b
Yue Wang <ywang@didichuxing.com>
2019-01-09 11:56:59 +0800

    init ip hubot

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+export BEARYCHAT_HUBOT_TOKEN=<your bearychat hubot token>
+npm start
+```
+
+Send your hubot a message that includes string "ip" (ignore case), it will reply its IP address.
---
+++ index.js
@@ -0,0 +1,37 @@
+const bearychat = require('bearychat');
+const RTMClient = require('bearychat-rtm-client');
+const RTMClientEvents = RTMClient.RTMClientEvents;
+const WebSocket = require('ws');
+const ip = require('ip');
+
+const token = process.env.BEARYCHAT_HUBOT_TOKEN;
+
+const client = new RTMClient({
+    url: function () {
+        return bearychat.rtm.start({ token })
+            .then(function (resp) { return resp.json() })
+            .then(function (data) { return data.ws_host });
+    },
+    WebSocket: WebSocket
+});
+
+client.on(RTMClientEvents.ONLINE, function () {
+    console.log('RTM online');
+});
+
+client.on(RTMClientEvents.OFFLINE, function () {
+    console.log('RTM offline');
+});
+
+client.on(RTMClientEvents.EVENT, function (message) {
+    if (message.type === 'message') {
+        const text = message.text || '';
+        if (text.toLowerCase().includes('ip')) {
+            client.send({
+                type: 'message',
+                vchannel_id: message.vchannel_id,
+                text: ip.address(),
+            });
+        }
+    }
+});
